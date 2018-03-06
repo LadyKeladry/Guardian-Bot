4ae00d6bbf7e2ec984681476adfc6c8c0fbffe43
LadyKeladry <32954095+LadyKeladry@users.noreply.github.com>
2018-03-06 22:08:23 +0100

    Add files via upload

---
+++ login.py
@@ -0,0 +1,5 @@
+#Token always has priority, if token is defined it will always attempt to login using a token
+#Comment the token line or set it empty to use email login
+token = 'MzY4ODc5NDI3NDYwOTIzNDEy.DMQbsw.ShQGOu4Ef6Q0uK10Gp8KQHRia1o'
+email = ''
+password = ''
